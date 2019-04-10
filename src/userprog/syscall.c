92f69f834ffee6a84a1c62dd014a11d97e69203d
RHowe22 <robjhowe22@gmail.com>
2019-04-10 19:49:14 -0400

    skeleton for syscalls

--- src/userprog/syscall.c
+++ src/userprog/syscall.c
@@ -3,7 +3,7 @@
 #include <syscall-nr.h>
 #include "threads/interrupt.h"
 #include "threads/thread.h"
-
+#include "lib/user/syscall.h"
 static void syscall_handler (struct intr_frame *);
 
 void
@@ -17,4 +17,39 @@ syscall_handler (struct intr_frame *f UNUSED)
 {
   printf ("system call!\n");
   thread_exit ();
+}
+
+void halt (void){
+
+}
+void exit (int status){
+
+}
+pid_t exec (const char *  cmd_line){
+  return 0;
+}
+int wait (tid_t pid);
+bool create (const char * file, unsigned initial_size){
+  return false;
+}
+bool remove (const char * file ){
+  return false;
+}
+int open (const char * file){
+  return -1;
+}
+int filesize (int fd){
+  return -1;
+}
+int write (int fd, const void * buffer, unsigned size){
+  return -1;
+}
+void seek (int fd, unsigned postion){
+
+}
+unsigned tell (int fd){
+  return 0;
+}
+void close (int fd){
+
 }
