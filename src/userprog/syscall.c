10f4d57813cf837a22470fe2f5a09575af90dbf3
Harshita <harshitagirase97@gmail.com>
2019-04-12 15:54:32 -0400

    Sys handler no buenhandler 1% implemented

--- src/userprog/syscall.c
+++ src/userprog/syscall.c
@@ -3,8 +3,24 @@
 #include <syscall-nr.h>
 #include "threads/interrupt.h"
 #include "threads/thread.h"
+#include "lib/user/syscall.h"
+#include "devices/shutdown.h"
 
 static void syscall_handler (struct intr_frame *);
+ 
+void _exit (int status);
+static void _halt (void);
+static pid_t _exec (const char *file);
+static int _wait (pid_t);
+static bool _create (const char *file, unsigned initial_size);
+static bool _remove (const char *file);
+static int _open (const char *file);
+static int _filesize (int fd);
+static int _read (int fd, void *buffer, unsigned length);
+static int _write (int fd, const void *buffer, unsigned length);
+static void _seek (int fd, unsigned position);
+static unsigned _tell (int fd);
+static void _close (int fd);
 
 void
 syscall_init (void) 
@@ -15,6 +31,26 @@ syscall_init (void)
 static void
 syscall_handler (struct intr_frame *f UNUSED) 
 {
+  int * esp = (int)f->esp;
+
+  int syscall_num = *esp;
+  switch (syscall_num) {
+    case SYS_HALT: halt();
+                   break;
+
+    case SYS_EXEC: 
+                   break;
+
+    case SYS_WRITE: break;
+  }
+
   printf ("system call!\n");
   thread_exit ();
+}
+
+
+static void
+_halt (void)
+{
+  shutdown_power_off ();
 }
